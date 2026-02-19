9dd298a716a5716e7257e4bb8d34c3d6f96371fb
Trang <dothuytrang2312@gmail.com>
2026-02-19 02:04:43 +0100

    Refactor, clean codes and remove repeat fetchFilms()

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
+const labelDisplay = document.getElementById("displayed-films-label");
+const searchBox = document.getElementById("search");
+const selectedFilm = document.getElementById("film-selector");
+const selectedShow = document.getElementById("show-selector");
+const showLink = "https://api.tvmaze.com/shows";
+const main = document.querySelector("main");
+
 let state = {
-  showslist: [],
+  showsList: [],
   films: [],
   searchText: "",
   selectedEpisode: "default",
@@ -7,45 +14,41 @@ let state = {
   selectedShow: "82",
 };
 
-
 const fetchShows = async () => {
-  const showlink = "https://api.tvmaze.com/shows";
-    const response = await fetch(showlink);
-    if(!response.ok) {
-      throw new Error("Error Fetching Shows"); 
-    }
-    // const shows = await response.json();
-    // console.log("shows:", + shows);
-    return await response.json();
+  const response = await fetch(showLink);
+  if(!response.ok) {
+    throw new Error("Error Fetching Shows"); 
+  }
+  // const shows = await response.json();
+  // console.log("shows:", + shows);
+  return await response.json();
 }
 
 fetchShows()
   .then((showArray) => {
-    state.showslist = showArray;
+    state.showsList = showArray;
     render();
-    makeshowSelectionAdded();
+    addShowSelection();
   }) 
   .catch((error) => {
     console.log(error);
     document.querySelector("main").innerHTML = "Failed to load data. Please try again later";
   });
 
-  function makeshowSelectionAdded() {
-  const selectshowContainer = document.getElementById("show-selector");
-  selectshowContainer.innerHTML = '<option value="default">Choose a Show</option>';
+// make show selector
+function addShowSelection() {
+  selectedShow.innerHTML = '<option value="default">Choose a Show</option>';
 
-  sortShowsAlphabetically(state.showslist).forEach((show) => {
+  sortShowsAlphabetically(state.showsList).forEach((show) => {
     const option = document.createElement("option");
     option.value = String(show.id);
     option.textContent = show.name;
-    selectshowContainer.appendChild(option);
+    selectedShow.appendChild(option);
   });
 }
 
-
-
 const fetchFilms = async () => {
-  const url = "https://api.tvmaze.com/shows/82/episodes";
+  const url = `https://api.tvmaze.com/shows/${state.selectedShow}/episodes`;
   const response = await fetch(url);
   if (!response.ok) {
     throw new Error(`Response status: ${response.status}`);
@@ -57,7 +60,7 @@ fetchFilms()
   .then((films) => {
     state.films = films;
     render();
-    makeSelection();
+    addFilmSelection();
   })
   .catch((error) => {
     console.log(error);
@@ -65,19 +68,15 @@ fetchFilms()
       "Failed to load data. Please try again later.";
   });
 
-//SELECTION FUNCTION FILM
-function makeSelection() {
-  const selectfilmContainer = document.getElementById("film-selector");
-
+// make film selector
+function addFilmSelection() {
   state.films.forEach((film) => {
     const option = document.createElement("option");
     option.value = film.name;
     option.textContent = `S${film.season.toString().padStart(2, "0")}E${film.number.toString().padStart(2, "0")} - ${film.name}`;
-    selectfilmContainer.appendChild(option);
+    selectedFilm.appendChild(option);
   });
 }
-
-
 
 function makeFilmCard({ name, season, number, image, summary }) {
   const filmCardDiv = document.createElement("div");
@@ -108,20 +107,15 @@ function makeFilmCard({ name, season, number, image, summary }) {
 }
 
 const render = () => {
-  const main = document.querySelector("main");
   main.innerHTML = "";
 
-  if (state.selectedShow === "default") {
-  const labeldisplay = document.getElementById("displayed-films");
-  labeldisplay.textContent = "Displaying 0/0";
-
-  if (!state.isInitialLoad) {
-    main.innerHTML = "";
-  }
-
-  return;
-}
-
+  if (state.selectedShow === "none") {
+    labelDisplay.textContent = "Displaying 0/0";
+    if (!state.isInitialLoad) {
+      main.innerHTML = "";
+    }
+    return; 
+    }
 
   if (state.inputContainer === "default") {
     const filterText = state.films.filter(
@@ -133,8 +127,7 @@ const render = () => {
     const filterFilms = filterText.map(makeFilmCard);
     main.append(...filterFilms);
 
-    const labeldisplay = document.getElementById("displayed-films");
-    labeldisplay.textContent = `Displaying ${filterText.length}/${state.films.length}`;
+    labelDisplay.textContent = `Displaying ${filterText.length}/${state.films.length}`;
   } else if (state.inputContainer === "select") {
     const filterSelected = state.films.filter(
       (film) => film.name === state.selectedEpisode,
@@ -143,24 +136,20 @@ const render = () => {
     const filterSelectedEpisode = filterSelected.map(makeFilmCard);
     main.append(...filterSelectedEpisode);
 
-    const labeldisplay = document.getElementById("displayed-films");
-    labeldisplay.textContent = `Displaying ${filterSelectedEpisode.length}/${state.films.length}`;
+    labelDisplay.textContent = `Displaying ${filterSelectedEpisode.length}/${state.films.length}`;
   }
 };
 
 render();
 
-const searchBox = document.getElementById("search");
 const handleSearch = (event) => {
   state.searchText = event.target.value;
   state.inputContainer = "default";
   render();
 };
-
 searchBox.addEventListener("input", handleSearch);
 
-//handleSelection
-const selected = document.getElementById("film-selector");
+// handleSelection
 const handleSelection = (event) => {
   state.selectedEpisode = event.target.value;
   if (state.selectedEpisode === "default") {
@@ -168,34 +157,20 @@ const handleSelection = (event) => {
   } else state.inputContainer = "select";
   render();
 };
+selectedFilm.addEventListener("change", handleSelection);
 
-selected.addEventListener("change", handleSelection);
-
-
-//getting the films by ID per show
-const fetchFilmsByShow = async (showId) => {
-  const url = `https://api.tvmaze.com/shows/${showId}/episodes`;
-  const response = await fetch(url);
-  if (!response.ok) {
-    throw new Error(`Response status: ${response.status}`);
-  }
-  return await response.json();
-};
-
-//sorting shows
+// sorting shows
 function sortShowsAlphabetically(shows) {
   return [...shows].sort((a, b) =>
     a.name.localeCompare(b.name, undefined, { sensitivity: "base" }),
   );
 }
 
-//reseting the selector
+// reseting the selector
 function resetEpisodeSelector() {
-  const selectfilmContainer = document.getElementById("film-selector");
-  selectfilmContainer.innerHTML = '<option value="default">Choose an episode</option>';
+  selectedFilm.innerHTML = '<option value="default">Choose an episode</option>';
 }
 
-const showSelected = document.getElementById("show-selector");
 const handleShowSelection = async (event) => {
   const showId = event.target.value;
   state.selectedShow = showId;
@@ -208,22 +183,21 @@ const handleShowSelection = async (event) => {
   state.selectedShow = "82";    //showing the same episodes from the very first time the page loads.
 
   try {
-    state.films = await fetchFilmsByShow("82");
+    state.films = await fetchFilms;
     resetEpisodeSelector();
-    makeSelection();
+    addFilmSelection();
     render();
   } catch (error) {
     console.log(error);
   }
-
   return;
-}
+  }
 
 
   try {
-    state.films = await fetchFilmsByShow(showId);
+    state.films = await fetchFilms();
     resetEpisodeSelector();
-    makeSelection();
+    addFilmSelection();
     render();
   } catch (error) {
     console.log(error);
@@ -232,7 +206,6 @@ const handleShowSelection = async (event) => {
   }
 };
 
-showSelected.addEventListener("change", handleShowSelection);
+selectedShow.addEventListener("change", handleShowSelection);
 
-
-makeshowSelectionAdded();
+addShowSelection();
